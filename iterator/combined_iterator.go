2f8466ad4600399ca03e61b4db1aee71db53c4e2
Maha Hajja <maha.d.hajja@gmail.com>
2025-01-23 12:58:25 -0800

    address review

--- iterator/combined_iterator.go
+++ iterator/combined_iterator.go
@@ -79,7 +79,7 @@ func NewCombinedIterator(
 			IteratorType:      position.TypeCDC,
 			Time:              now,
 			AfterSnapshot:     true,
-			SequenceNumberMap: make(map[string]string),
+			SequenceNumberMap: map[string]string{},
 		})
 		if err != nil {
 			return nil, fmt.Errorf("could not create the CDC iterator: %w", err)
